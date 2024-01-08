fba9a634f71863442a312b5320470d06c5f6ec0a
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-01-08 19:11:21 -0400

    add scalar types

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -5,13 +5,23 @@ const typeDefs = `
   type Query {
     hello: String
     getPerson(name: String, age: Int): String
+    getInt(age: Int): Int
+    getFloat(price: Float): Float
+    getString: String
+    getBoolean: Boolean
+    getID: ID
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => 'hola mundo',
-    getPerson: (_, args) => `Hello ${args.name}, you ${args.age} years old?`
+    getPerson: (_, args) => `Hello ${args.name}, you ${args.age} years old?`,
+    getInt: (_, args) => args.age,
+    getFloat: (_, args) => args.price,
+    getString: () => "Word",
+    getBoolean: () => 1 === 1,
+    getID: () => "u1232432"
   }
 }
 
