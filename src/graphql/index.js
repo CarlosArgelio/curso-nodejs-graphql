d2eebcb317379a88aff58897f8636a26868b3caf
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-01-10 12:27:46 -0400

    add login

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -2,11 +2,13 @@ const { ApolloServer } = require('apollo-server-express')
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core')
 const { loadFiles } = require('@graphql-tools/load-files')
 const resolvers = require('./resolvers')
+const { buildContext } = require('graphql-passport');
 
 const useGraphql = async (app) => {
   const server = new ApolloServer({
     typeDefs: await loadFiles('./src/**/*.graphql'),
     resolvers,
+    context:({ req, res }) => buildContext({ req, res }),
     playgroud: true,
     plugins: [
       ApolloServerPluginLandingPageGraphQLPlayground
